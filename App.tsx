67bfbf9a4874acad68ec868d7307e677a2b682e8
alrafat <89281033+alrafat@users.noreply.github.com>
2025-10-26 15:35:49 +0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
-return (
-  <div className="p-8 bg-gray-100 text-black">
-    <h1 className="text-4xl font-bold">React Works!</h1>
-    <p>If you see this text, React is rendering correctly!</p>
-  </div>
-);
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-8 bg-gray-100 text-black">
+      <h1 className="text-4xl font-bold">React Works!</h1>
+      <p>If you see this text, React is rendering correctly!</p>
+    </div>
+  );
+}
+
+export default App;
