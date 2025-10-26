504c71c6970b77b0fe65b430b2440b16598e8b3c
alrafat <89281033+alrafat@users.noreply.github.com>
2025-10-26 14:40:25 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  define: {
-    // Vite does not expose process.env to the client by default.
-    // This line replaces instances of process.env.API_KEY in the code
-    // with the actual environment variable value at build time.
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  server: {
+    port: 5173
   }
 });
